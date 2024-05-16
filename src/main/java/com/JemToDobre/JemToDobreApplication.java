c9c16d27a16b9d175b6cfc56ef2af22762a44e39
JakMajer <115946773+JakMajer@users.noreply.github.com>
2024-05-16 16:32:15 +0200

    css

--- src/main/java/com/JemToDobre/JemToDobreApplication.java
+++ src/main/java/com/JemToDobre/JemToDobreApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class JemToDobreApplication {
 
 	public static void main(String[] args) {
